9bc6bec86479bd9fe9c52e484b93ec9d4be5a488
mayone <mayone321@gmail.com>
2022-07-15 18:36:57 +0800

    [Feature] Add Web3 Onboard Provider

---
+++ src/network.tsx
@@ -0,0 +1,84 @@
+import React, { useEffect, useContext, useState } from 'react';
+import type { OnboardAPI } from '@web3-onboard/core';
+import { useConnectWallet, useWallets } from '@web3-onboard/react';
+import { useWeb3 } from './web3';
+
+interface ContextData {
+  web3Onboard?: OnboardAPI;
+}
+
+interface ContextActions {
+  action?: any;
+}
+
+type Context = [ContextData, ContextActions];
+
+const NetworkContext = React.createContext<Context>([
+  // { defaultContract },
+  {},
+  {},
+]);
+
+export const NetworkProvider: React.FC<{children: any}> = ({ children }) => {
+  // const [{web3Onboard}] = useWeb3();
+  const [{ wallet, connecting }, connect, disconnect] = useConnectWallet();
+  const connectedWallets = useWallets();
+  const [walletInitialized, setWalletInitialized] = useState(false);
+
+  useEffect(() => {
+    const prevConnectedWalletsStr = window.localStorage.getItem('connectedWallets');
+    const prevConnectedWallets = prevConnectedWalletsStr ? JSON.parse(prevConnectedWalletsStr) : null;
+
+    if (prevConnectedWallets?.length) {
+      (async () => {
+        // await connect({
+        //   autoSelect: prevConnectedWallets[0],
+        // });
+        for (const walletLabel of prevConnectedWallets.reverse()) {
+          await connect({
+            autoSelect: {
+              label: walletLabel,
+              disableModals: true,
+            },
+          });
+        }
+        setWalletInitialized(true);
+      })();
+    } else {
+      (async () => {
+        await connect();
+        setWalletInitialized(true);
+      })();
+    }
+  }, [connect]);
+
+  useEffect(() => {
+    if (!walletInitialized) return;
+
+    const connectedWalletsLabelArray = connectedWallets.map(
+      ({ label }) => label
+    )
+    window.localStorage.setItem(
+      'connectedWallets',
+      JSON.stringify(connectedWalletsLabelArray),
+    )
+
+    if (!connectedWallets.length) {
+      connect();
+    }
+  }, [connectedWallets, walletInitialized, connect]);
+
+  // if (connecting) {
+  //   return null;
+  // }
+
+  return (
+    <>
+      {children}
+    </>
+  )
+}
+
+export function useNetwork() {
+  return useContext(NetworkContext);
+}
---
+++ src/web3.tsx
@@ -0,0 +1,48 @@
+import React, { useEffect, useContext, useState } from 'react';
+import type { OnboardAPI } from '@web3-onboard/core';
+import initWeb3Onboard from './initWeb3Onboard';
+
+interface ContextData {
+  web3Onboard?: OnboardAPI;
+}
+
+interface ContextActions {
+  action?: any;
+}
+
+type Context = [ContextData, ContextActions];
+
+const Web3Context = React.createContext<Context>([
+  // { defaultContract },
+  {},
+  {},
+]);
+
+export const Web3Provider: React.FC<{children: any}> = ({ children }) => {
+  const [web3Onboard, setWeb3Onboard] = useState<OnboardAPI>();
+
+  useEffect(() => {
+    setWeb3Onboard(initWeb3Onboard);
+  }, []);
+
+  if (!web3Onboard) return <div>Loading...</div>;
+
+  return (
+    <Web3Context.Provider
+      value={[
+        {
+          web3Onboard,
+        },
+        {
+          // Actions
+        },
+      ]}
+    >
+      {children}
+    </Web3Context.Provider>
+  );
+};
+
+export function useWeb3() {
+  return useContext(Web3Context);
+}
