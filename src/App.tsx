41ce6f3ee94a7c652f3ebf117d88c72c2ecdd217
mayone <mayone321@gmail.com>
2022-07-19 19:25:03 +0800

    [Change] Use high order component

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,50 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Web3Provider } from './web3';
 import { NetworkProvider } from './network';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+// const wrapHOC = <P,>(WrappedComponent: React.ComponentType<P>) => (props: P) => {
+//   return (
+//     <Web3Provider>
+//       <NetworkProvider>
+//         <WrappedComponent {...props} />
+//       </NetworkProvider>
+//     </Web3Provider>
+//   );
+// }
+
+const wrapHOC = (WrappedComponent: React.FC) => ({...props}) => {
   return (
     <Web3Provider>
       <NetworkProvider>
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-              Edit <code>src/App.tsx</code> and save to reload.
-            </p>
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-          </header>
-        </div>
+        <WrappedComponent {...props} />
       </NetworkProvider>
     </Web3Provider>
   );
 }
 
-export default App;
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+const WrappedApp = wrapHOC(App)
+
+export default WrappedApp;
