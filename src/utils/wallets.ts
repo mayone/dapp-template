04440214b319648d4ef05a39f2a5ea29831ad51a
mayone <mayone321@gmail.com>
2022-09-28 23:28:01 +0800

    [Change] Fix indentation

--- src/utils/wallets.ts
+++ src/utils/wallets.ts
@@ -1,34 +1,34 @@
 import type { ConnectOptions, WalletState } from '@web3-onboard/core';
 
 export const updateWallets = (
-    connectedWallets: WalletState[],
+  connectedWallets: WalletState[],
 ) => {
-    const connectedWalletsLabelArray = connectedWallets.map(
-        ({ label }) => label
-    );
-    window.localStorage.setItem(
-        'connectedWallets',
-        JSON.stringify(connectedWalletsLabelArray),
-    );
+  const connectedWalletsLabelArray = connectedWallets.map(
+    ({ label }) => label
+  );
+  window.localStorage.setItem(
+    'connectedWallets',
+    JSON.stringify(connectedWalletsLabelArray),
+  );
 }
 
 export const reConnectWallets = async (
-    connect: (options?: ConnectOptions) => Promise<WalletState[]>,
-    onComplete?: () => void,
+  connect: (options?: ConnectOptions) => Promise<WalletState[]>,
+  onComplete?: () => void,
 ) => {
-    const prevConnectedWalletsStr = window.localStorage.getItem('connectedWallets');
-    const prevConnectedWallets = prevConnectedWalletsStr ? JSON.parse(prevConnectedWalletsStr) : null;
+  const prevConnectedWalletsStr = window.localStorage.getItem('connectedWallets');
+  const prevConnectedWallets = prevConnectedWalletsStr ? JSON.parse(prevConnectedWalletsStr) : null;
 
-    if (prevConnectedWallets?.length) {
-        for (const walletLabel of prevConnectedWallets.reverse()) {
-            await connect({
-                autoSelect: {
-                    label: walletLabel,
-                    disableModals: true,
-                },
-            });
-        }
+  if (prevConnectedWallets?.length) {
+    for (const walletLabel of prevConnectedWallets.reverse()) {
+      await connect({
+        autoSelect: {
+          label: walletLabel,
+          disableModals: true,
+        },
+      });
     }
+  }
 
-    onComplete && onComplete();
+  onComplete && onComplete();
 };
